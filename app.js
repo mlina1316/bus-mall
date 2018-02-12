6ca023f1d18f7439042d591682f5c4ec7c295096
Laboni A <asthanal@yahoo.com>
2018-02-12 12:59:20 -0800

    busmall with results showing

--- app.js
+++ app.js
@@ -2,20 +2,17 @@
 
 //array to store the objects
 var allProducts = [];
-var currentImages = [];
-var previousImages = [];
-var repeat = false;
-//counter for set of 3 images displayed at a time
+var previousShown = [];
 var counter = 0;
-
-//make my constructor function
-function Product(name, filepath) {
-  this.name = name;
-  this.filepath = filepath;
-  this.clicked=0;
-  this.display=0;
-  allProducts.push(this);
-}
+var firstRandom;
+var secondRandom;
+var thirdRandom;
+var imgEl1 = document.getElementById('image-one');
+var imgEl2 = document.getElementById('image-two');
+var imgEl3 = document.getElementById('image-three');
+var total = document.getElementById('total');
+var numLoops = 25;
+var displayed = false;
 
 //use my constructor function to create new Product instances
 new Product('Bag', 'images/bag.jpg');
@@ -36,100 +33,227 @@ new Product('Usb', 'images/usb.gif');
 new Product('Water-can', 'images/water-can.jpg');
 new Product('Wine-glass', 'images/wine-glass.jpg');
 
-function randomIndex() {
-  var randomIndex = Math.floor(Math.random() * allProducts.length);
-  return randomIndex;
+showRandomImages();
+
+//make my constructor function
+function Product(name, filepath) {
+  this.name = name;
+  this.filepath = filepath;
+  this.shown = 0;
+  this.clicked = 0;
+
+  allProducts.push(this);
 }
 
-function repeatImg(imgEl) {
-  for (var i=0; i<currentImages.length; i++) {
-    if (currentImages[i] === imgEl.src) {
-      repeat = true;
+function randomProductIndex() {
+  var randomIndex = Math.random() * allProducts.length;
+  var roundedRandomIndex = Math.floor(randomIndex);
+  return roundedRandomIndex;
+}
+
+function showRandomImages() {
+
+  firstRandom = null;
+  secondRandom = null;
+  thirdRandom = null;
+
+  // Generate first image.
+  var unique = false;
+  var newImageIndex = null;
+
+  while (!unique) {
+    // Generate new index and check against previous 3 images.
+    newImageIndex = randomProductIndex();
+
+    // Set Unique to true as we start with the assumption that we got a unique
+    // index and will try to negate that.
+    unique = true;
+
+    // Check against previous three images. If there is a match then index is
+    // not unique.
+    for (var i = 0; i < previousShown.length; i++) {
+      if (previousShown[i].filepath === allProducts[newImageIndex].filepath) {
+        unique = false;
+        break;
+      }
+    }
+    // If Unique is still true then we have found the first image.
+    if (unique === true) {
+      firstRandom = allProducts[newImageIndex];
+      console.log('firstRandom: ', firstRandom);
       break;
     }
   }
 
-  if (repeat === false){
-    for (i=0; i<previousImages.length; i++){
-      if (previousImages[i] === imgEl.src) {
-        repeat = true;
+  // Generate second image.
+  unique = false;
+
+  while (!unique) {
+    // Generate new index and check against previous 3 images + firstRandom.
+    newImageIndex = randomProductIndex();
+
+    // Set Unique to true as we start with the assumption that we got a unique
+    // index and will try to negate that.
+    unique = true;
+
+    // Check against previous three images. If there is a match then index is
+    // not unique.
+    for (var j = 0; j < previousShown.length; j++) {
+      if (previousShown[j].filepath === allProducts[newImageIndex].filepath) {
+        unique = false;
         break;
       }
+    }
+    // Also check against firstRandom for uniqueness.
+    if (firstRandom.filepath === allProducts[newImageIndex].filepath) {
+      unique = false;
+    }
+
+    // If Unique is still true then we have found the second image.
+    if (unique === true) {
+      secondRandom = allProducts[newImageIndex];
+      console.log('secondRandon: ', secondRandom);
+      break;
+    }
+  }
+
+  // Generate third image.
+  unique = false;
+  while (!unique) {
+    // Generate new index and check against previous 3 images + firstRandom and secondRandom.
+    newImageIndex = randomProductIndex();
+
+    // Set Unique to true as we start with the assumption that we got a unique
+    // index and will try to negate that.
+    unique = true;
+
+    // Check against previous three images. If there is a match then index is
+    // not unique.
+    for (var k = 0; k < previousShown.length; k++) {
+      if (previousShown[k].filepath === allProducts[newImageIndex].filepath) {
+        unique = false;
+        break;
+      }
+    }
+    if ((firstRandom.filepath === allProducts[newImageIndex].filepath) ||
+      (secondRandom.filepath === allProducts[newImageIndex].filepath)) {
+      unique = false;
+    }
+
+    // If Unique is still true then we have found the third image.
+    if (unique === true) {
+      thirdRandom = allProducts[newImageIndex];
+      console.log('thirdRandom: ', thirdRandom);
+      break;
+    }
+  }
+
+  // We should now have 3 unique images which were not repeated the last time.
+  // Display them
+  imgEl1.setAttribute('src', firstRandom.filepath);
+  imgEl2.setAttribute('src', secondRandom.filepath);
+  imgEl3.setAttribute('src', thirdRandom.filepath);
+
+  counter++;
+
+  // Udpate previous shown & update counts.
+  previousShown = [];
+  previousShown.push(firstRandom);
+  previousShown.push(secondRandom);
+  previousShown.push(thirdRandom);
+  firstRandom.shown++;
+  secondRandom.shown++;
+  thirdRandom.shown++;
+
+} // EOF - showRandomImages.
+
+imgEl1.addEventListener('click', function () {
+  event.preventDefault();
+  firstRandom.clicked++;
+
+  if (counter < numLoops) {
+    showRandomImages();
+  }
+  else {
+    disableImageOnClick();
+    calculateResults();
+  }
+});
+
+imgEl2.addEventListener('click', function () {
+  event.preventDefault();
+  secondRandom.clicked++;
+
+  if (counter < numLoops) {
+    showRandomImages();
+  }
+  else {
+    disableImageOnClick();
+    calculateResults();
+  }
+});
+
+imgEl3.addEventListener('click', function () {
+  event.preventDefault();
+  thirdRandom.clicked++;
+
+  if (counter < numLoops) {
+    showRandomImages();
+  }
+  else {
+    disableImageOnClick();
+    calculateResults();
+  }
+});
+
+function disableImageOnClick(){
+  imgEl1.onclick = null;
+  imgEl2.onclick = null;
+  imgEl3.onclick = null;
+}
+
+function makeHeaderRow(total) {
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
+
+  thEl.textContent = 'Product Name';
+  trEl.appendChild(thEl);
+
+  thEl = document.createElement('th');
+  thEl.textContent = 'Number of times Image Displayed';
+  trEl.appendChild(thEl);
+
+  thEl = document.createElement('th');
+  thEl.textContent = 'Number of times Image Clicked';
+  trEl.appendChild(thEl);
+
+  total.appendChild(trEl);
+  console.log('Making header row');
+}
+
+function calculateResults() {
+  if (displayed === false) {
+    makeHeaderRow(total);
+
+    for (var l = 0; l < allProducts.length; l++) {
+      var trEl = document.createElement('tr');
+      var tdEl_productName = document.createElement('td');
+      var tdEl_numDisplayed = document.createElement('td');
+      var tdEl_clicked = document.createElement('td');
+
+      tdEl_productName.textContent = allProducts[l].name;
+      tdEl_numDisplayed.textContent = allProducts[l].shown;
+      tdEl_clicked.textContent = allProducts[l].clicked;
+
+      trEl.appendChild(tdEl_productName);
+      trEl.appendChild(tdEl_numDisplayed);
+      trEl.appendChild(tdEl_clicked);
+      console.log('Calculating results');
+      total.appendChild(trEl);
+
+      //this is to handle multiple click events from displaying
+      displayed = true;
     }
   }
 }
 
-function showProducts() {
-  for (var j=0; j<3; j++){
-    var rand = randomIndex();
-    var imgEl = document.getElementById('product-pic' + (j+1));
-
-    imgEl.src = allProducts[rand].filepath;
-    repeatImg(imgEl);
-
-    // if (repeat === true) {
-    //   j--;
-    // }
-    // else {
-    // imgEl.onclick = function () {
-    //   allProducts[rand].clicked++;
-    //   counter++;
-    // };
-    currentImages.push(imgEl.src);
-    allProducts[rand].display++;
-    // }
-    console.log(repeat);
-    repeat = false;
-  }
-  console.log(counter);
-}
-var threeprods = document.getElementById('threeprods');
-threeprods.addEventListener('click', showProducts);
-
-
-
-// {
-//   event.preventDefault();
-//   allProducts[rand].clicked++;
-//   // showProducts();
-// });
-
-
-//   if (j===3){
-//previousImages = currentImages;
-//     counter++;
-//     console.log(counter);
-//   }
-// }
-
-//  function showResultsTable() {
-//  var trEl = document.createElement('tr');
-//  var thEl = document.createElement('th');
-//  var total = document.getElementById('total');
-//  thEl.textContent = 'Product Name';
-//  trEl.appendChild(thEl);
-
-//  thEl = document.createElement('th');
-//  thEl.textContent = 'Number of times Displyed';
-
-//  thEl = document.createElement('th');
-//  thEl.textContent = 'Number of times Clicked';
-
-//  for(var k=0; k<allProducts.length; k++){
-//    trEl = document.createElement('tr');
-//    var tdEL = document.createElement('td');
-
-//    tdEL.textContent = allProducts[k].name;
-//    trEl.appendChild(tdEL);
-
-//    tdEl= document.createElement('td');
-//    tdEL.textContent = allProducts[k].display;
-
-//    tdEl= document.createElement('td');
-//    tdEL.textContent = allProducts[k].clicked;
-
-//    total.appendChild('tr');
-//  }
-
-showProducts();
-// showResultsTable()
